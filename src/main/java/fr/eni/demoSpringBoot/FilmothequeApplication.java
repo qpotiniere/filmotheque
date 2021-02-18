09c63be3edadea19a9ce234140b2cf4ff692323d
qpotiniere <quentyn.potyniere@hotmail.fr>
2021-02-18 12:03:02 +0100

    Premier commit

---
+++ src/main/java/fr/eni/demoSpringBoot/FilmothequeApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.demoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmothequeApplication.class, args);
+	}
+
+}
