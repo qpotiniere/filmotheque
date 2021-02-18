f18a9c7e96fe5f3b1150536d9b09341607f75cd2
tbarrier <theo.barrier@hotmail.fr>
2021-02-18 14:16:46 +0100

    bo class

---
+++ src/main/java/fr/eni/bo/Acteur.java
@@ -0,0 +1,19 @@
+package fr.eni.bo;
+
+
+public class Acteur extends Participant {
+
+	public Acteur() {
+		super();
+	}
+
+	public Acteur(int id, String nom, String prenom) {
+		super(id, nom, prenom);
+	}
+
+	public Acteur(String nom, String prenom) {
+		super(nom, prenom);
+	}
+
+	
+}
