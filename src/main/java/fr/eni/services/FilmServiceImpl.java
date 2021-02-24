1722f6725962a7e0810d2842b9db27e2540ebf93
qpotiniere <quentyn.potyniere@hotmail.fr>
2021-02-24 10:11:11 +0100

    recupération

--- src/main/java/fr/eni/services/FilmServiceImpl.java
+++ src/main/java/fr/eni/services/FilmServiceImpl.java
@@ -9,21 +9,10 @@ import fr.eni.bo.Participant;
 
 @Service
 public class FilmServiceImpl implements FilmService {
-
-	@Override
-	public Film ajouterUnFilm() {
-		return null;
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public Film detailFilm(int idFilm) {
-		return listFilm().get(idFilm);
-	}
-
-	@Override
-	public ArrayList<Film> listFilm() {
-		ArrayList<Film> films = new ArrayList<Film>();
+	
+	ArrayList<Film> films = new ArrayList<Film>();
+	
+	public ArrayList<Film> lesFilms(){
 		ArrayList<Participant> acteurs = new ArrayList<Participant>();
 		acteurs.add(new Participant("DiCaprio", "Leonardo"));
 		acteurs.add(new Participant("Winselt", "Kate"));
@@ -31,6 +20,27 @@ public class FilmServiceImpl implements FilmService {
 		films.add(new Film(1,"Titanic1", 2005, new Genre(1,"romantic"), new Participant("James", "Cameron") , "1h30 min", acteurs , "En 1997, l'épave du Titanic est l'objet d'une exploration fièvreuse"));
 		films.add(new Film(2,"Titanic2", 2005, new Genre(1,"romantic"), new Participant("James", "Cameron") , "1h30 min", acteurs , "En 1997, l'épave du Titanic est l'objet d'une exploration fièvreuse"));
 		return films;
+	}
+
+	@Override
+	public ArrayList<Film> ajouterUnFilm() {
+		ArrayList<Participant> acteurs = new ArrayList<Participant>();
+		acteurs.add(new Participant("DiCaprio", "Leonardo"));
+		acteurs.add(new Participant("Winselt", "Kate"));
+		Film film = new Film(3,"Titanic3", 2005, new Genre(1,"romantic"), new Participant("James", "Cameron") , "1h30 min", acteurs , "En 1997, l'épave du Titanic est l'objet d'une exploration fièvreuse");
+		lesFilms().add(film);
+		return lesFilms();
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public Film detailFilm(int idFilm) {
+		return lesFilms().get(idFilm);
+	}
+
+	@Override
+	public ArrayList<Film> listFilm() {
+		return lesFilms();
 	}
 	
 	//A faire si le temps
